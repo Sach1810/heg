5253cafbbcf14f07c850af4471bb029fb6b4324e
Sacha David <Sach1810@Sachas-MacBook-Pro.local>
2016-06-04 23:00:58 +0300

    Home page complete (few bit of css to edit) started with ui router but having issues

---
+++ public/components/nav/nav.js
@@ -0,0 +1,24 @@
+$(window).resize(function(){
+  if ($(window).width() >= 650 & $('#mobile-nav').is(':visible')){  
+    $('#mobile-nav').slideUp('fast');
+  } 
+});
+
+$('#bars').click(function(){
+
+if ($('#mobile-nav').is(':visible')){
+  $('#mobile-nav').slideUp('slow');
+} else {
+  $('#mobile-nav').slideDown('slow');
+}
+
+});
+
+function fix()
+{
+    var el = this;
+    var par = el.parentNode;
+    var next = el.nextSibling;
+    par.removeChild(el);
+    setTimeout(function() {par.insertBefore(el, next);}, 0)
+}
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
-var heg = angular.module('heg', []);
+var heg = angular.module('heg', ['ui.router']);
+
+heg.config(function($stateProvider, $urlRouterProvider) {
+  
+  // $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html'
+  })
+});
