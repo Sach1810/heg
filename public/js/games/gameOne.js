517a0c3515b2fcc8ee02968856897d957a59f07d
Sacha David <Sach1810@sachas-mbp.mynet>
2016-06-08 17:48:10 +0300

    updating css on gameOne

--- public/js/games/gameOne.js
+++ public/js/games/gameOne.js
@@ -1,11 +1,12 @@
 var socket = io.connect();
 
 var countInterval = 1;
-var startTime = 1;
+var startTime = 3;
 var countdownTime = startTime;
 
 var squareChangeSpeed = 3000;
-var totalPlayingTime = 60;
+var totalPlayingTime = 5;
+var gameDuration = 30;
 
 var computerId;
 var phoneId;
@@ -28,7 +29,7 @@ var playerTwoScore = {
   wrong: 0
 }
 
-var phoneLink = "https://4e20de04.ngrok.io/gameonecontroller";
+var phoneLink = "https://heg.herokuapp.com/gameonecontroller";
 
 $('#qrcode').qrcode({
   "size": 100,
@@ -223,11 +224,11 @@ var startGame = function(){
 
 setTimeout(function(){
     clearInterval(changeSquares);
-    // $("#allCubes").addClass('hide');
-    // $('.liveScore').addClass('hide');
+    $("#cube").addClass('hide');
+    $('.liveScore').addClass('hide');
 
 
-    // reset();
+    reset();
 
   }, totalPlayingTime * 1000);
 };
@@ -247,80 +248,14 @@ var gameTime = function(){
   }, 1000);
 };
 
+var reset = function(){
+  socket.disconnect();
+  inPlay = false;
+  countdownTime = startTime;
+  totalPlayingTime = gameDuration;
+  $("#endGame").removeClass('hide');
+  $("#gameTime").html(" ");
+  $("#countdown").html(" ");
 
-// ********************************
+};
 
-// var socket = io.connect();
-
-// var countInterval = 1;
-// var startTime = 3;
-// var countdownTime = startTime;
-
-// var phoneLink = "https://4e20de04.ngrok.io/gameonecontroller";
-
-// $('#qrcode').qrcode({
-//   "size": 100,
-//   "color": "#3a3",
-//   "text": phoneLink});
-
-// $("#webAddress").html(phoneLink);
-
-// var register = function(qty) {
-//   $('#buttonContainer').addClass('hide');
-//   $('#qrContainer').removeClass('hide');
-//   var playerQty = qty;
-//   socket.emit('qtyPlayers', playerQty);
-//   console.log(playerQty);
-// };
-
-// socket.on('result', function(gameOne){
-//   console.log('result');
-//   if (gameOne.playerOne) {
-//       $('#playerConnected').html('Player 1 connected');
-//   }
-  
-//   if (gameOne.playerTwo) {
-//       $('#playerConnected').html('Both players connected starting game');
-//   }
-
-//   if (gameOne.start) {
-//     $('#qrContainer').addClass('hide');
-//     $('#countdown').removeClass('hide');
-//     countdown();
-//   }
-
-//   });
-
-// var countdown = function(){
-//   console.log("Start Game");
-
-//   var timeTillStart = setInterval(function(){
-//   countdownTime -= countInterval;
-//   $("#countdown").html(countdownTime);
-  
-//   if (countdownTime === 0) {
-//     $("#countdown").addClass('hide');
-//     $("#cube").removeClass('hide');
-      
-//       clearInterval(timeTillStart);
-      
-//       startGame();
-//     };
-
-//   },1000); 
-// }
-
-// var startGame = function(){
-//   socket.on('phoneData', function(coordinates){
-
-//   document.getElementById('cube').style.webkitTransform = 
-//     document.getElementById('cube').style.transform =
-//       'rotateX(' + coordinates.az + 'deg) ' +
-//       'rotateY(' + coordinates.gy + 'deg) ' +
-//       'rotateZ(' + coordinates.bx + 'deg)';
-// });
-
-// }
-
-
-// if player one turn player 1 green and append 2 players to waiting for second player.
